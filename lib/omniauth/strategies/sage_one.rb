5ddc84592523b389ee2a66ca06ff8a9c78af0987
Mike Heft <mikeheft@gmail.com>
2019-07-27 11:15:39 +0100

    Remove comments relating to countries as sage no longer distinguishes between countries when using their api

--- lib/omniauth/strategies/sage_one.rb
+++ lib/omniauth/strategies/sage_one.rb
@@ -27,9 +27,6 @@ module OmniAuth
         }
       end
 
-      # SageOne has different token endpoints for each available country. The country is returned in
-      # the authorization callback. Configure the OAuth client to use that information by
-      # customizing the client options
       def client
         ::OAuth2::Client.new(options.client_id, options.client_secret, client_options)
       end
@@ -52,8 +49,6 @@ module OmniAuth
         request.env ? request[:country].try(:downcase) : options.client_options[:country]
       end
 
-      # Override client_options[:token_url] using the country from ether the request or the
-      # provided country option
       def client_options
         options.client_options[:token_url] = 'https://oauth.accounting.sage.com/token'
         deep_symbolize(options.client_options)
